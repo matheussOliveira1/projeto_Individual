3fe3a95cca4e33c64551e5877886f75cba3fe059
matheussOliveira1 <matheus.aoliveira@sptech.school>
2022-06-02 14:57:21 -0300

    att site indiv

--- site/public/SCRIPT_MODEL/script_WEBdemon.sql
+++ site/public/SCRIPT_MODEL/script_WEBdemon.sql
@@ -1,14 +1,29 @@
 CREATE DATABASE webdemon;
 USE webdemon;
 
-CREATE TABLE Cadastro(
-	idCadastro INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(90),
-    sobrenome VARCHAR(90),
-    email VARCHAR(90),
-    senha VARCHAR(90),
-    persFav VARCHAR(90) CONSTRAINT chkPersona CHECK (persFav = 'Asta' OR persFav = 'Liebe' OR persFav = 'Yuno')
+CREATE TABLE Personagem(
+	idPersonagem INT PRIMARY KEY AUTO_INCREMENT,
+    nomePersonagem VARCHAR(90) CONSTRAINT chkPersona CHECK (nomePersonagem = 'Asta' OR nomePersonagem = 'Liebe' OR nomePersonagem = 'Yuno')
 );
 
-INSERT INTO Cadastro VALUES
-(NULL, 'Matheus', 'Alves', 'matheus@matheus.com', 'teste', 'Asta');
+INSERT INTO Personagem VALUES
+(NULL, 'Asta'),
+(NULL, 'Liebe'),
+(NULL, 'Yuno');
+
+CREATE TABLE Formulario(
+	idCadastro INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(90),
+    idade INT,
+    sexo VARCHAR(9) CONSTRAINT chkSexo CHECK (sexo = 'masculino' OR sexo = 'feminino' OR sexo = 'outro'),
+    fkPersFav INT,
+    FOREIGN KEY (fkPersFav) REFERENCES Personagem (idPersonagem)
+);
+
+select * from personagem;
+
+-- SELECTS QUE QUERO UTILIZAR:
+-- HISTORICO UTILIZANDO UM 'SELECT *' COM ORDER BY id desc;
+-- QUANTIDADE DE FORMULARIOS OU SEJA UM SELECT COUNT DOS ID;
+-- PEGAR O PERSONAGEM MAIS VOTADO;
+-- (TALVEZ) PEGAR A IDADE MEDIA DAS VOTAÇÕES;
